d72b1f538678694bc08373a328f585d2047a47e8
danil0711 <danilmitsenko21@gmail.com>
2026-03-03 23:24:46 +0500

    logger set

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,5 +1,5 @@
 # app/api/auth.py
-
+from app.core.logger import logger
 from fastapi import APIRouter, Depends, HTTPException
 from app.api.crud.user import authenticate_user, create_user, get_user_by_email
 from app.infrastructure.db.get_db import get_db
@@ -12,39 +12,42 @@ from app.core.security import create_access_token
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
+
 @router.post("/register", response_model=Token)
 async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
+    logger.info(f"Register attempt: {user.email}")
 
     existing_user = await get_user_by_email(db, user.email)
     if existing_user:
+        logger.warning(f"Register failed: email already exists ({user.email})")
         raise HTTPException(status_code=400, detail="Такой email уже зарегистрирован.")
 
-    db_user = await create_user(db, user.email, user.password)
+    try:
+        db_user = await create_user(db, user.email, user.password)
+        access_token = create_access_token({"sub": str(db_user.id)})
+        logger.info(f"User registered successfully id={db_user.id}")
+        return {"access_token": access_token, "token_type": "bearer"}
+    except Exception as e:
+        logger.exception(f"Register failed due to internal error: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
 
-    access_token = create_access_token({"sub": str(db_user.id)})
 
-    return {
-        "access_token": access_token,
-        "token_type": "bearer"
-    }
-    
-    
 @router.post("/login")
 async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: AsyncSession = Depends(get_db),
 ):
+    logger.info(f"Login attempt: {form_data.username}")
+
     user = await authenticate_user(db, form_data.username, form_data.password)
-
     if not user:
-        raise HTTPException(
-            status_code=401,
-            detail="Неверный email или пароль",
-        )
+        logger.warning(f"Login failed: invalid credentials ({form_data.username})")
+        raise HTTPException(status_code=401, detail="Неверный email или пароль")
 
-    access_token = create_access_token({"sub": str(user.id)})
-
-    return {
-        "access_token": access_token,
-        "token_type": "bearer",
-    }
+    try:
+        access_token = create_access_token({"sub": str(user.id)})
+        logger.info(f"Login success: user_id={user.id}")
+        return {"access_token": access_token, "token_type": "bearer"}
+    except Exception:
+        logger.exception("Unexpected error while creating access token")
+        raise HTTPException(status_code=500, detail="Internal server error")

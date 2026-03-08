d02aff8fb2f8d5dd4d283631747208727018b84a
danil0711 <danilmitsenko21@gmail.com>
2026-03-08 20:02:16 +0500

    scheduler starter in main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.logger import logger
 from app.api.routers.auth import router as auth_router
 from app.api.routers.users import router as users_router
 from app.api.routers.tasks import router as tasks_router
 from app.infrastructure.db.health import check_db_connection
+from app.services.scheduler.scheduler import start_scheduler
 
 
 @asynccontextmanager
@@ -15,11 +17,16 @@ async def lifespan(app: FastAPI):
         logger.critical("Cannot connect to database. Exiting.")
         os._exit(1)
 
+    logger.info("Starting scheduler")
+    start_scheduler()
+
     yield
+
     logger.info("Application shutdown")
 
 
 app = FastAPI(lifespan=lifespan)
+
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(tasks_router)

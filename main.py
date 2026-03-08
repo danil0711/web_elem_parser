794d5feacbe137d8b9685716dd06a0eabf9d8c69
danil0711 <danilmitsenko21@gmail.com>
2026-03-08 20:02:26 +0500

    cors middleware

--- main.py
+++ main.py
@@ -26,7 +26,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(tasks_router)

b87cd4d373bf477eeab50a6d61606832461f769b
danil0711 <danilmitsenko21@gmail.com>
2026-03-18 22:08:33 +0500

    feat: next_run_at in taskread schema

--- app/api/crud/schema.py
+++ app/api/crud/schema.py
@@ -5,24 +5,27 @@ from uuid import UUID
 from pydantic import BaseModel, HttpUrl, field_serializer
 from typing import Optional
 
+
 class TaskBase(BaseModel):
-    
     url: HttpUrl
     selector: str
     interval: int = 600
     condition: Optional[str] = None
     duration: Optional[int] = None
-    
+
 
 class TaskCreate(TaskBase):
     pass
 
+
 class TaskRead(BaseModel):
     id: int
-    user_id: UUID           
+    user_id: UUID
     is_active: bool
     created_at: datetime
     last_run_at: Optional[datetime] = None
+    next_run_at: datetime
+
     url: HttpUrl
     selector: str
     interval: int
@@ -35,7 +38,11 @@ class TaskRead(BaseModel):
     @field_serializer("created_at")
     def serialize_created_at(self, v: datetime, **kwargs):
         return v.isoformat()  # автоматически отдаёт строку JSON
-    
+
     @field_serializer("last_run_at")
     def serialize_last_run_at(self, v: Optional[datetime], **kwargs):
         return v.isoformat() if v else None
+
+    @field_serializer("next_run_at")
+    def serialize_next_run_at(self, v: datetime, **kwargs):
+        return v.isoformat()

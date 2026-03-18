e431d2111cbecd66c8bb2a2282b8fea83db00d9c
danil0711 <danilmitsenko21@gmail.com>
2026-03-18 22:11:48 +0500

    feat: singleton fetcher usage

--- app/services/monitoring/monitoring.py
+++ app/services/monitoring/monitoring.py
@@ -1,21 +1,25 @@
 from datetime import datetime, UTC
 from app.infrastructure.db.models.task import Task
-from app.infrastructure.fetchers.async_fetcher import HttpxAsyncFetcher
+from app.infrastructure.fetchers.async_fetcher import fetcher
 from app.services.condition.condition import ConditionEvaluator
-from sqlalchemy.ext.asyncio import AsyncSession
 from app.services.parser.parser import ParserService
 
-async def run_task(db: AsyncSession, task: Task):
-    fetcher = HttpxAsyncFetcher()
+
+# TODO при хендле ошибки отключать задачу.
+async def run_task(task: Task):
+    html = await fetcher.fetch(task.url)
     parser = ParserService()
 
     html = await fetcher.fetch(task.url)
+    # получаем значение с сайта по селектору
     extracted_value = parser.extract_text(html, task.selector)
 
     should_alert = False
 
     if task.condition:
-        should_alert = ConditionEvaluator.check_condition(task.condition, extracted_value)
+        should_alert = ConditionEvaluator.check_condition(
+            task.condition, extracted_value
+        )
 
     if should_alert:
         print(f"ALERT for task {task.id}")
